edcf0df1f65a6675166971f87ff7eca2b123bf2f
kornchaval <kornchaval@gmail.com>
2017-03-30 17:25:32 +0700

    Test using v-if, v-else, e-for and v-on

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+
+<div>
+  <input v-model="name">
+  <p v-if="name && name !== 'korn'">Hi, {{ name }}</p>
+  <p v-else-if="name === 'korn'">:D</p>
+  <p v-else>No Data</p>
+  <p v-for="x in list"> {{ x + ' ' +name }}</p>
+  <button v-on:click="sayHello">Hello</button>
+  <button @click="sayHello">@click</button>
+</div>
+
+
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data () {
+    return {
+      name: '',
+      list: [
+        1, 2, 3, 4, 5
+      ]
+    }
+  },
+  methods: {
+    sayHello () {
+      window.alert('Im,' + this.name)
+    }
+  }
 }
 </script>
 
