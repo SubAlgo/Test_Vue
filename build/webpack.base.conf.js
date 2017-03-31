5f3409b2f27078270275628d162b794d3568f6b4
kornchaval <kornchaval@gmail.com>
2017-03-31 23:41:12 +0700

    Test use JQuery and Disable Auto open Web browser

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
+const webpack - require('webpack')
 var vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
@@ -63,5 +64,12 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'window.$': 'jquery',
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ]
 }
